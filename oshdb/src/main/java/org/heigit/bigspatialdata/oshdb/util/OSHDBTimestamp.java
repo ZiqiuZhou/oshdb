f654adf6eca0d540bb751e81035430e1597cdb4e
Martin Raifer <martin.raifer@heigit.org>
2020-06-19 15:21:16 +0200

    implement hasCode to be consistent with overridden equals

--- oshdb/src/main/java/org/heigit/bigspatialdata/oshdb/util/OSHDBTimestamp.java
+++ oshdb/src/main/java/org/heigit/bigspatialdata/oshdb/util/OSHDBTimestamp.java
@@ -6,13 +6,14 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Objects;
 
 public class OSHDBTimestamp implements Comparable<OSHDBTimestamp>, Serializable {
   private static final long serialVersionUID = 1L;
-  private long _tstamp;
+  private long tstamp;
 
   public OSHDBTimestamp(long tstamp) {
-    this._tstamp = tstamp;
+    this.tstamp = tstamp;
   }
 
   public OSHDBTimestamp(Date tstamp) {
@@ -20,32 +21,37 @@ public class OSHDBTimestamp implements Comparable<OSHDBTimestamp>, Serializable 
   }
   
   public void setTimestamp(long tstamp){
-    this._tstamp = tstamp;
+    this.tstamp = tstamp;
   }
 
   @Override
   public int compareTo(OSHDBTimestamp other) {
-    return Long.compare(this._tstamp, other._tstamp);
+    return Long.compare(this.tstamp, other.tstamp);
   }
 
   @Override
   public boolean equals(Object other) {
     if (other instanceof OSHDBTimestamp)
-      return this._tstamp == ((OSHDBTimestamp)other)._tstamp;
+      return this.tstamp == ((OSHDBTimestamp)other).tstamp;
     else
       return super.equals(other);
   }
+
+  @Override
+  public int hashCode() {
+    return Long.hashCode(this.tstamp);
+  }
   
   public Date toDate() {
-    return new Date(this._tstamp * 1000);
+    return new Date(this.tstamp * 1000);
   }
 
   public long getRawUnixTimestamp() {
-    return this._tstamp;
+    return this.tstamp;
   }
 
   public String toString() {
-    ZonedDateTime zdt = ZonedDateTime.ofInstant(Instant.ofEpochSecond(_tstamp), ZoneOffset.UTC);
+    ZonedDateTime zdt = ZonedDateTime.ofInstant(Instant.ofEpochSecond(tstamp), ZoneOffset.UTC);
     return zdt.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
   }
 }
